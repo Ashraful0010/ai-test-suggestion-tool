075e4953324fcdd9bc09b9e508d204895945bcda
Ashraful Islam Opu <ashrafulislamopu20@gmail.com>
2025-07-18 20:44:25 +0600

    updated codes

---
+++ dashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Tester Dashboard</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <div class="main-wrapper">
+        <div class="card">
+            <h1>Tester Dashboard</h1>
+            <p>Select a simulated test page:</p>
+            <div class="button-group">
+                <a href="test-pages/test_login.php" class="btn green">Login Page</a>
+                <a href="test-pages/test_signup.php" class="btn orange">Signup Page</a>
+                <a href="test-pages/test_formfill.php" class="btn blue">Form Fill Page</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
